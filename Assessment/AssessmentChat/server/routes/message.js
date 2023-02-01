aeb949de82efd654fb143fa186594a31606c58c1
Loki-SD <michaelloquias96@gmail.com>
2023-02-01 21:13:19 +0800

    last commit

--- Assessment/AssessmentChat/server/routes/message.js
+++ Assessment/AssessmentChat/server/routes/message.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const { addMessage, getMessages } = require("../controllers/messageController");
 
-router.post("/addMessage/", addMessage);
-router.post("/getMessages/", getMessages);
+router.post("/addMessage", addMessage);
+router.post("/getMessages", getMessages);
 
 module.exports = router;
